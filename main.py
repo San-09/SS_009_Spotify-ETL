d7d3b28d6f603c278692e56cdd67da6657fa76d3
theep26 <santheep@hotmail.co.uk>
2021-08-13 15:10:25 +0100

    Data to Database
    Create table in database if it doesnt exist, save data into database

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import pandas as pd
-import sqlalchemy
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
 import requests
 import json
 from datetime import datetime
 import datetime
-#import mysql.connector
+import mysql.connector
+from mysql.connector import errorcode
+
 
 #Pandas Settings: show more columns in terminal
 pd.set_option('display.max_columns', 5)
 
-# cnx = mysql.connector.connect(user="root", password="", host="3306", database="")
-# MySQL_location = ""
 
 User_id = "Santheep Sritharan"
-token = "BQCWKb65I0jsWD_1rAoEl1FP_iVXj7m9ac_iGhKfigWaTo_tW6hU1XUfx6_wh9Vet5-pFojKtX8X1IYjezxG_28Cc_YXEPLuVk8j2SsoB0ptP7DTMTkQfhVlHGH72lvZ_Tv9mvWP86TMtWcmOCJyk0-XLoQGoQTuo8QvE6vp"
+token = "BQCu6GEchEd6XeKUByvh2ZsrZ_jZqICGQeWG6LM-hmsAnPn5fRYheYpwyxHiE8sdsSqhqPoL2aJQYH_VThMi3N02OUfqQ0XtW3znFGYakp4ulAW8hknVf3AIqVa-xVg7wzh1gAVtyF7G-GeICb-SvX7EorZdGHnrezhvk2gv"
 
 def validate_data(df: pd.DataFrame) -> bool:
     #Check if dataframe is empty
@@ -55,10 +55,16 @@ if __name__ == "__main__":
     yesterday = today - datetime.timedelta(days=1)
     yesterday_unix_timestamp = int(yesterday.timestamp())*1000
 
-    r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(
-        time=yesterday_unix_timestamp), headers = headers)
+    try:
+        r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(
+            time=yesterday_unix_timestamp), headers = headers)
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print("Invalid spotify token error:", str(r))
+        raise SystemExit(err)
 
     data = r.json()
+    print("Data retrieved")
 
 #print(data)
 
@@ -93,7 +99,74 @@ dict_columns = ["song_title", "artist_name", "release_date", "played_at", "times
 
 song_df = pd.DataFrame(song_dict, columns = dict_columns)
 
+#Run data validation
 if validate_data(song_df):
     print("Data valid")
 
+# Load data to the database
+# engine = sqlalchemy.create_engine(Mysql_url)    #Create a new engine instance
+# conn = sql
+#
+# def CONNECT_TO_DB():
+#     try:
+#         cnx = mysql.connector.connect(user='root', password='xxxxx',
+#                               host='localhost',
+#                               database='spotify')
+#     except mysql.connector.Error as err:
+#         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#             print("Something is wrong with your user name or password")
+#         elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#             print("Database does not exist")
+#         else:
+#             print(err)
+#     else:
+#         cnx.close()
+
+
+TABLES = {}
+TABLES['spotify_playlist'] = (
+    "CREATE TABLE 'spotify_playlist' ("
+    " 'song_name' VARCHAR(200),"
+    " 'artist_name' VARCHAR(200),"
+    " 'release_date' DATE,"
+    " 'played_at' VARCHAR(200),"
+    " 'timestamp' VARCHAR(200),"
+    " CONSTRAINT primary_key_constraint PRIMARY KEY ('played_at')"
+    ") ENGINE=InnoDB")
+
+cnx = mysql.connector.connect(user='root', password='xxxxx',
+                              host='localhost',
+                              database='spotify')
+
+cursor = cnx.cursor()
+
+for table_name in TABLES:
+    table_description = TABLES[table_name]
+    try:
+        print("Creating table {}: ".format(table_name), end='')
+        cursor.execute(table_description)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("already exists.")
+        else:
+            print(err.msg)
+    else:
+        print("OK")
+
+# Create SQLAlchemy engine to connect to MySQL Database
+engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
+				.format(host="localhost", db="spotify", user="root", pw="xxxx"))
+
+try:
+    song_df.to_sql("spotify_playlist", engine, index=False, if_exists='append')
+except:
+    print("Data already exists in the database")
+
+#Make sure data is committed to the database
+cnx.commit()
+
+cursor.close()
+cnx.close()
+
 print(song_df)
+
